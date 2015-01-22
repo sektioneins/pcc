410f065c51b32bf5d9fa12f3b1132fe01770afe6
Ben Fuhrmannek <ben@sektioneins.de>
2015-01-22 16:22:32 +0100

    updated release date for update check

--- phpconfigcheck.php
+++ phpconfigcheck.php
@@ -52,9 +52,9 @@
 /*****************************************************************************/
 
 $pcc_name = "PHP Secure Configuration Checker";
-$pcc_version = "0.1-dev3";
+$pcc_version = "0.1-dev4";
 $pcc_copy = "(c) 2014 SektionEins GmbH / Ben Fuhrmannek";
-$pcc_date = "2014-11-15"; // release date for update check
+$pcc_date = "2015-01-22"; // release date for update check
 $pcc_url = "https://github.com/sektioneins/pcc"; // download URL
 
 /*****************************************************************************/
